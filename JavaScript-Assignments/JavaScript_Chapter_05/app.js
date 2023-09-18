b3fbe99ac8117cfb47825113523e55fc9010b913
Fahad.Hassan <fahadfuuast2@gmail.com>
2023-09-18 23:47:17 +0500

    JS_Assignment_Chapter_1 to 5_Uploaded

---
+++ JavaScript-Assignments/JavaScript_Chapter_05/app.js
@@ -0,0 +1,216 @@
+
+// ==== Question no 01 ==== //
+
+var numOne = 3;
+var numTwo = 5;
+var totalNum = (numOne + numTwo);
+alert(totalNum);
+
+// ==== Question no 02 ==== //
+
+var totalNum = (numTwo - numOne);
+alert(totalNum);
+
+var totalNum = (numTwo * numOne);
+alert(totalNum);
+
+var totalNum = (numTwo / numOne);
+alert(totalNum);
+
+var totalNum = (numOne % numTwo);
+alert(totalNum);
+
+// ==== Question no 03 ==== //
+
+// var a = "undifined";
+
+// document.write("Value after variable declaration is: " + a);
+
+// initial value 5
+
+var a = 5;
+alert(a);
+var incrementValue = (++a);
+alert(incrementValue);
+
+var addition = 7;
+
+var sum = (incrementValue + addition);
+alert(sum);
+
+var subtraction = (--sum);
+alert(subtraction);
+
+var division = (subtraction % 3);
+alert(division);
+
+// ==== Question no 04 ==== //
+
+var oneMovieTicket = 600;
+var buyingTickets = 5;
+
+var totalCost = (oneMovieTicket * buyingTickets);
+alert(totalCost);
+
+// ==== Question no 05 ==== //
+
+// Table of Four //
+
+var four = 4;
+var one = 1;
+var fourOne = (four * one);
+alert(fourOne);
+
+var four = 4;
+var two = 2;
+var fourTwo = (four * two);
+alert(fourTwo);
+
+var four = 4;
+var three = 3;
+var fourThree = (four * three);
+alert(fourThree);
+
+var four = 4;
+var foour = 4;
+var fourFour = (four * foour);
+alert(fourFour);
+
+var four = 4;
+var five = 5;
+var fourFive = (four * five);
+alert(fourFive);
+
+var four = 4;
+var six = 6;
+var fourSix = (four * six);
+alert(fourSix);
+
+var four = 4;
+var seven = 7;
+var fourSeven = (four * seven);
+alert(fourSeven);
+
+var four = 4;
+var eight = 8;
+var fourEight = (four * eight);
+alert(fourEight);
+
+var four = 4;
+var nine = 9;
+var fourNine = (four * nine);
+alert(fourNine);
+
+var four = 4;
+var ten = 10;
+var fourTen = (four * ten);
+alert(fourTen);
+
+// ==== Question no 06 ==== //
+
+//Data:
+
+var c = 25;
+var f = 70;
+
+// For Fahrenhite Conversion using Celclius temp value
+
+// var cValue = (c * 9) / 5 + 32;
+
+// Solution;
+// c = 25
+// put it into a formula;
+
+var multiplication = 9;
+var division = 5;
+var constant = 32;
+var fahrenhite = ((c * multiplication) / division + constant);
+alert(fahrenhite);
+
+// For Celcius Conversion using Fahrehnhite temp value
+
+// var fValue = ((f - 32) * 5) / 9;
+
+// Solution;
+// f = 70
+// put it into a formula;
+
+var divisions = 9;
+var multiplicationn = 5;
+var constants = 32;
+var celcius = (((f - constants) * multiplicationn) / divisions);
+alert(celcius);
+
+// ==== Question no 07 ==== //
+
+var priceOfItem1 = 650;
+var qtyOfItem1 = 3;
+var priceOfItem2 = 100;
+var qtyOfItem2 = 7;
+var shippingCharges = 100;
+
+var totalOrderCost = ((priceOfItem1 * qtyOfItem1) + (priceOfItem2 * qtyOfItem2) + shippingCharges);
+alert(totalOrderCost);
+
+// ==== Question no 08 ==== //
+
+//Data:
+// totalMarks: 980;
+// marksObtained: 804;
+
+var marksObtained = 804;
+var totalMarks = 980;
+var percentage = ((marksObtained / totalMarks) * 100);
+alert(percentage);
+
+// ==== Question no 09 ==== //
+
+//Data:
+// 10 US Dollars each 104.80 pkr.
+// 25 Saudi Riyals each 28 pkr.
+
+var us = 10;
+var usToPkr = 104.80;
+var sr = 25;
+var srToPkr = 28;
+
+var totalCurrencyInPkr = ((us * usToPkr) + (sr * srToPkr));
+alert(totalCurrencyInPkr);
+
+// ==== Question no 10 ==== //
+
+// var num =5;
+// document.write(number + 5 + number * 10 + number / 2);
+
+// ==== Question no 11 ==== //
+
+var currentYear = 2016;
+var birthYear = 1992;
+var age = (currentYear - birthYear);
+alert(age);
+
+// ==== Question no 12 ==== //
+
+// var radius = 20;
+// var circumference = 2 * Math.PI * radius;
+// var area = Math.PI * Math.pow(radius, 2);
+
+// document.write("<h1>The Geometrizer</h1>");
+// document.write(" <br /> Radius of a circle: " + radius);
+// document.write(" <br /> The circumference is " + circumference);
+// document.write(" <br /> The area is " + area);
+
+// ==== Question no 13 ==== //
+
+var favSnack = "choclate chip";
+var currentAge = 15;
+var maxAge = 65;
+var perDaySnacks = 3;
+
+//   " <br /> You will need " +
+
+var lifeTimeChipNeed = ((maxAge - currentAge) * 3);
+alert(lifeTimeChipNeed);
+
+
+// The End //
