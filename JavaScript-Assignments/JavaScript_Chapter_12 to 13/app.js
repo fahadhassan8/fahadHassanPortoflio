ca3be4a248d8653caad0634887e545bf632577d9
Fahad.Hassan <fahadfuuast2@gmail.com>
2023-09-25 00:32:51 +0500

    JavaScript_Chapter_12 to 13_Uploaded

---
+++ JavaScript-Assignments/JavaScript_Chapter_12 to 13/app.js
@@ -0,0 +1,94 @@
+// Chapter 12 to 13
+
+//Question 01
+
+// var userInput = prompt("Enter a value");
+
+// if(userInput >= 40 && userInput <= 64){
+//     alert("Its a number");
+// }
+// else if(userInput >= 65 && userInput <= 90){
+//     alert("Its an Uppercase Letter");
+// }
+// else if(userInput >= 97 && userInput <= 122){
+//     alert("Its a Lowercase Letter");
+// }
+// else{
+//     alert("Invalid Input");
+// }
+
+// Question 02
+
+// var integerOne = prompt("Enter First Integer");
+// var integerTwo = prompt("Enter Second Integer");
+
+// if(integerOne > integerTwo){
+//     alert(integerOne + " Is greater Than " + integerTwo);
+// }
+// else if(integerOne === integerTwo){
+//     alert("Both integers are Equal");
+// }
+// else{
+//     alert(integerTwo + " Is greater Than " + integerOne);
+// }
+
+// Question 03
+
+// var userInputNum = prompt("Enter a number", 2);
+
+// if(userInputNum > 0){
+//     alert("Input value is positive");
+// }
+// else if(userInputNum < 0){
+//     alert("Input value is negative");
+// }
+// else {
+//     alert("Input value is zero");
+// }
+
+// Question 05
+
+// var userPswrd = prompt("Create new password");
+// var validPswrd = prompt("Enter a Password");
+
+// if(userPswrd === "" || validPswrd === ""){
+//     alert("Please enter your password")
+// }
+// else if(userPswrd === validPswrd){
+//     alert("Correct! The password you entered matches the original password");
+// }
+// else{
+//     alert("Incorrect password");
+// }
+
+// Question 06
+
+// var greeting;
+// var hour = 13;
+
+// if(hour < 18){
+//     alert("Good day");
+// }
+// else{
+//     alert("Good evening");
+// }
+
+// Question 07
+
+// var clockTime = prompt("What o'clock is it");
+
+// if(clockTime >= "0000" && clockTime < 1200){
+//     alert("Good Morning")
+// }
+// else if(clockTime >= 1200 && clockTime < 1700){
+//     alert("Good afternoon");
+// }
+// else if(clockTime >= 1700 && clockTime < 2100){
+//     alert("Good evening");
+// }
+// else if(clockTime >= 2100 && clockTime < 2159){
+//     alert("Good night");
+// }
+// else{
+//     alert("Enter correct time")
+// }
